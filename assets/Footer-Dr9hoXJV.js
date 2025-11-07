6d011cb324634b0cac329fb88ac8a4ff53e28a4c
Anthony Brian Freay <anthonyfreay@gmail.com>
2025-11-07 00:31:23 -0500

    Updates

--- assets/Footer-Dr9hoXJV.js
+++ assets/Footer-Dr9hoXJV.js
@@ -1,4 +1,4 @@
-import{g as vt,r as a,R as P,c as q,j as f,L as M}from"./index-BYGUf56L.js";import{I as Jt}from"./Icons-DBMBcHhQ.js";var Oe={exports:{}};/*!
+import{g as vt,r as a,R as P,c as q,j as f,L as M}from"./index-d1jGNHIn.js";import{I as Jt}from"./Icons-fvr94-DI.js";var Oe={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
---
+++ assets/PageHeader-36vs4rxA.js
@@ -0,0 +1,1 @@
+import{j as e,H as t}from"./index-d1jGNHIn.js";const a=({title:r})=>e.jsx(t,{children:e.jsx("title",{children:r})});export{a as P};
